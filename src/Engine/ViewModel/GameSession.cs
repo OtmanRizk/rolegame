41df5e03fa07fdd0922dd4b6201c19ee3d470ae5
OtmanRizk <rizkotman@gmail.com>
2025-10-01 14:57:06 +0200

    feat: added game session and player class

---
+++ src/Engine/ViewModel/GameSession.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Model;
+
+namespace Engine.ViewModel
+{
+    class GameSession
+    {
+        Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        { 
+            CurrentPlayer = new Player();
+        }
+
+
+
+    }
+}
