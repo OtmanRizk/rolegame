bdb899d2ab7694f556f9b46f41f9def27508ae69
OtmanRizk <49912227+OtmanRizk@users.noreply.github.com>
2025-11-18 21:17:06 +0100

    feat(): new ViewModel GameSession

---
+++ src/ViewModels/GameSession.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models.PlayerModel;
+
+namespace ViewModels.GameSession
+{
+    public class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        { 
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Gandalf";
+            CurrentPlayer.Gold = 1000000;
+            CurrentPlayer.CharacterClass = "Wizard";
+            CurrentPlayer.HitPoints = 100;
+            CurrentPlayer.ExperiencePoint = 5000;
+            CurrentPlayer.Level = 20;
+        }
+    }
+}
